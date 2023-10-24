73e9cf5405b88c0b38c56288320ec082dbb0ce0d
wabowen89 <wabowen89@gmail.com>
2023-10-23 22:00:02 -0700

    modified logs to better reflect motor activity/updated new array indices

--- b_motorControl.ino
+++ b_motorControl.ino
@@ -1,7 +1,7 @@
 void motorControl() {
 
   // Print to Serial Monitor
-  Serial.println("Reading motorcontrol values ");
+  Serial.println("control values ");
   
   // Read the Joystick Y position
   joyposVert = analogRead(joyVert); 
@@ -14,7 +14,7 @@ void motorControl() {
   {
     // This is Backward
     // Set Motors backward
-    motorcontrol[2] = 1;
+    motorcontrol[1] = 1;
  
     //Determine Motor Speeds
     // As we are going backwards we need to reverse readings
@@ -25,7 +25,7 @@ void motorControl() {
   {
     // This is Forward
     // Set Motors forward
-    motorcontrol[2] = 0;
+    motorcontrol[1] = 0;
  
     //Determine Motor Speeds
     motorcontrol[0] = map(joyposVert, 564, 1023, 0, 255);
@@ -35,58 +35,16 @@ void motorControl() {
   {
     // This is Stopped
     motorcontrol[0] = 0;
-    motorcontrol[1] = 0;
-    motorcontrol[2] = 0; 
+    motorcontrol[1] = 0; 
  
   }
-
-  // if (joyposHorz < 511) {
-  //   motorcontrol[3] = 1;
-
-  //   motorcontrol[1] = map(joyposHorz, 511, 1023, 90, 180);
-  // }
-  // else if (joyposHorz > 512) {
-  //   motorcontrol[3] = 0;
-
-  //   motorcontrol[1] = map(joyposHorz, 512, 1023, 90, 0)
-  // }
-  
  
   // Adjust to prevent "buzzing" at very low speed
-  if (motorcontrol[0] < 8)motorcontrol[0] = 0;
-  // if (motorcontrol[1] < 8)motorcontrol[1] = 0;
+  if (motorcontrol[0] < 25)motorcontrol[0] = 0;
+
  
   //Display the Motor Control values in the serial monitor.
-  int yValue = analogRead(joyVert);
-  Serial.print("Motor: ");
-  Serial.print(yValue);
-  Serial.println(motorcontrol[0]);
-  // Serial.print(" - Motor B: ");
-  // Serial.print(motorcontrol[1]);
-  // Serial.print(" - Direction: ");
-  // Serial.println(motorcontrol[2]);
-  
-  //Send a message containing Motor Control data to manager_server
-  if (RadioManager.sendtoWait(motorcontrol, sizeof(motorcontrol), SERVER_ADDRESS))
-  {
-    // Now wait for a reply from the server
-    uint8_t len = sizeof(buf);
-    uint8_t from;
-    if (RadioManager.recvfromAckTimeout(buf, &len, 2000, &from))
-    {
-      Serial.print("got reply from : 0x");
-      Serial.print(from, HEX);
-      Serial.print(": ");
-      Serial.println((char*)buf);
-    }
-    else
-    {
-      Serial.println("No reply, is nrf24_reliable_datagram_server running?");
-    }
-  }
-  else
-    Serial.println("sendtoWait failed");
- 
-  delay(100);  // Wait a bit before next transmission
+  Serial.print("Rear Motor: ");
+  Serial.print(motorcontrol[0]);
 
 }
