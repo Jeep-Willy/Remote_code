5fdeccca83c8666d4f1e56b4dcc777b50dffa670
wabowen89 <wabowen89@gmail.com>
2023-10-24 10:54:06 -0700

    added clearer comments/logs for continued testing

--- d_transmitData.ino
+++ d_transmitData.ino
@@ -1,12 +1,20 @@
+// Transmition of  joystick data to car
+
 void transmitData() {
+
+Serial.print("Server Address: ");
+Serial.println(SERVER_ADDRESS);
 
   //Send a message containing Motor/Servo Control data to manager_server
   if (RadioManager.sendtoWait(motorcontrol, sizeof(motorcontrol), SERVER_ADDRESS))
   {
+    Serial.println("RadioManager ACK RCVD");
+    
     // Now wait for a reply from the server
     uint8_t len = sizeof(buf);
     uint8_t from;
-    if (RadioManager.recvfromAckTimeout(buf, &len, 2000, &from))
+
+    if (RadioManager.recvfromAckTimeout(buf, &len, 1000, &from))
     {
       Serial.print(": ");
       Serial.println((char*)buf);
@@ -15,6 +23,7 @@ void transmitData() {
     {
       Serial.println("No reply, is nrf24_reliable_datagram_server running?");
     }
+
   }
   else
     Serial.println("sendtoWait failed");
