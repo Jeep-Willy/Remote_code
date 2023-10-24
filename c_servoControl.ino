88952c6ea701c7ff8834a246cd228f25222a29c2
wabowen89 <wabowen89@gmail.com>
2023-10-23 22:00:53 -0700

    replaced motorcontrol with servocontrol array. modified logs to better reflect servo activity

--- c_servoControl.ino
+++ c_servoControl.ino
@@ -1,6 +1,51 @@
-void servoContro() {
+void servoControl() {
+
 
   // Read the Joystick X position
   joyposHorz = analogRead(joyHorz);
+
+
+  // Determine if this is a left or right motion
+  // Do this by reading the Horizontal Value
+  // Apply results to Servo speed and to Direction
+
+  if (joyposHorz < 460) {
+
+    servocontrol[1] = 1;
+
+    //Determine Servo Speed
+    servocontrol[0] = map(joyposHorz, 460, 1023, 90, 180);
+
+    myServo.write(servocontrol[0]);
+
+    // Serial.print("Joystick values: ");
+    // Serial.print(joyposHorz);
+    // Serial.print(" Converted values: ");
+    // Serial.println(servocontrol[0]);
+  }
+  else if (joyposHorz > 564) 
+  {
+    servocontrol[1] = 0;
+
+    //Determine Servo Speed
+    servocontrol[0] = map(joyposHorz, 564, 0, 90, 0);
+
+    myServo.write(servocontrol[0]);
+
+    // Serial.print("Joystick values: ");
+    // Serial.print(joyposHorz);
+    // Serial.print(" Converted values: ");
+    // Serial.println(servocontrol[0]);
+  }
+    else
+  {
+    // This is Stopped
+    servocontrol[0] = 90;
+    servocontrol[1] = 0; 
+ 
+  }
+
+  Serial.print(" Servo Motor: ");
+  Serial.println(servocontrol[0]);
 
 }
