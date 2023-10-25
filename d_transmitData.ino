45b52300bc183fbef353ff3daf9ceb746fd2e93d
wabowen89 <wabowen89@gmail.com>
2023-10-25 14:54:20 -0700

    removed unnecessary logs. decreased delay to 10ms

--- d_transmitData.ino
+++ d_transmitData.ino
@@ -2,25 +2,21 @@
 
 void transmitData() {
 
-Serial.print("Server Address: ");
-Serial.println(SERVER_ADDRESS);
-
   //Send a message containing Motor/Servo Control data to manager_server
   if (RadioManager.sendtoWait(motorcontrol, sizeof(motorcontrol), SERVER_ADDRESS))
   {
+
     Serial.println("RadioManager ACK RCVD");
     
     // Now wait for a reply from the server
     uint8_t len = sizeof(buf);
     uint8_t from;
 
-    if (RadioManager.recvfromAckTimeout(buf, &len, 1000, &from))
-    {
+    if (RadioManager.recvfromAckTimeout(buf, &len, 1000, &from)) {
       Serial.print(": ");
       Serial.println((char*)buf);
     }
-    else
-    {
+    else {
       Serial.println("No reply, is nrf24_reliable_datagram_server running?");
     }
 
@@ -28,6 +24,6 @@ Serial.println(SERVER_ADDRESS);
   else
     Serial.println("sendtoWait failed");
  
-  delay(100);  // Wait a bit before next transmission
+  delay(10);  // Wait a bit before next transmission
 
 }
