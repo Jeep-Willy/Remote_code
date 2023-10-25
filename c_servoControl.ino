5eaca8cf8184d99881346b2e7ddfb6cb78138b88
wabowen89 <wabowen89@gmail.com>
2023-10-25 14:53:46 -0700

    removed unnecessary if/else statement. Directly mapped joystick values to servo angle in motorcontrol[2]

--- c_servoControl.ino
+++ c_servoControl.ino
@@ -5,39 +5,8 @@ void servoControl() {
   // Read the Joystick X position
   joyposHorz = analogRead(joyHorz);
 
-
-  // Determine if this is a left or right motion
-  // Do this by reading the Horizontal Value
-  // Apply results to Servo speed and to Direction
-
-  if (joyposHorz < 460) {
-
-    motorcontrol[3] = 1;
-
-    //Determine Servo Speed
-    motorcontrol[2] = map(joyposHorz, 460, 1023, 90, 180);
-
-    // myServo.write(motorcontrol[2]);
-
-  }
-  else if (joyposHorz > 564) 
-  {
-
-    motorcontrol[3] = 0;
-
-    //Determine Servo Speed
-    motorcontrol[2] = map(joyposHorz, 564, 0, 90, 0);
-
-    // myServo.write(motorcontrol[2]);
-
-  }
-    else
-  {
-    // This is Stopped
-    motorcontrol[2] = 90;
-    motorcontrol[3] = 0; 
- 
-  }
+  // convert joystick position data to angle for servo
+  motorcontrol[2] = map(joyposHorz, 0, 1023, 0, 180);
 
   Serial.print(" Servo Motor: ");
   Serial.println(motorcontrol[2]);
