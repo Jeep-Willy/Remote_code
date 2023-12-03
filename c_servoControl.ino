d32a98a7ee6f1f2c2c8c5bb454341312a2e852c4
wabowen89 <wabowen89@gmail.com>
2023-12-03 14:01:25 -0800

    calibrated turning radius range

--- c_servoControl.ino
+++ c_servoControl.ino
@@ -6,7 +6,7 @@ void servoControl() {
   joyposHorz = analogRead(joyHorz);
 
   // convert joystick position data to angle for servo
-  motorcontrol[2] = map(joyposHorz, 0, 1023, 0, 180);
+  motorcontrol[2] = map(joyposHorz, 0, 1023, 72, 94);
 
   Serial.print(" Servo Motor: ");
   Serial.println(motorcontrol[2]);
