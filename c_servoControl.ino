3cc93227ba521670573a3d9c9f5e6566b1a7b257
wabowen89 <wabowen89@gmail.com>
2023-12-03 21:33:02 -0800

    more calibration adjustments

--- c_servoControl.ino
+++ c_servoControl.ino
@@ -6,7 +6,7 @@ void servoControl() {
   joyposHorz = analogRead(joyHorz);
 
   // convert joystick position data to angle for servo
-  motorcontrol[2] = map(joyposHorz, 0, 1023, 78, 98);
+  motorcontrol[2] = map(joyposHorz, 0, 1023, 75, 102);
 
   Serial.print(" Servo Motor: ");
   Serial.println(motorcontrol[2]);
